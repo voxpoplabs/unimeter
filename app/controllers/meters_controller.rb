3dbab4ba3dab3ef46bca5b5a3cc8b9c129d68c55
Jonathan Salis <jon@voxpoplabs.com>
2020-04-28 21:47:47 -0400

    Removing signup and adding S3 bucket region

--- app/controllers/meters_controller.rb
+++ app/controllers/meters_controller.rb
@@ -233,7 +233,7 @@ class MetersController < ApplicationController
 	end
 
 	def set_s3_direct_post
-    	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-  	end
+  	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+	end
 
 end
