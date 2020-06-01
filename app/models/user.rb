9074a19bfbfca3f808459505e8d05c3655e67bc1
Jonathan Salis <jon@voxpoplabs.com>
2020-06-01 11:35:39 -0400

    Removing links from the footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,7 @@ class User < ApplicationRecord
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-												  BCrypt::Engine.cost
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
@@ -46,38 +45,38 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, nil)
 	end
 
-		# Activates an account.
-	  def activate
+	# Activates an account.
+  def activate
 		update_attribute(:activated,    true)
 		update_attribute(:activated_at, Time.zone.now)
-	  end
+  end
 
-	  # Sends activation email.
-	  def send_activation_email
+  # Sends activation email.
+  def send_activation_email
 		UserMailer.account_activation(self).deliver_now
-	  end
+  end
 
-	  # Sets the password reset attributes.
+  # Sets the password reset attributes.
 	def create_reset_digest
-	self.reset_token = User.new_token
-	update_attribute(:reset_digest,  User.digest(reset_token))
-	update_attribute(:reset_sent_at, Time.zone.now)
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest,  User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
 	end
 
 	# Sends password reset email.
 	def send_password_reset_email
-	UserMailer.password_reset(self).deliver_now
+		UserMailer.password_reset(self).deliver_now
 	end
 
 	# Returns true if a password reset has expired.
 	def password_reset_expired?
-	reset_sent_at < 2.hours.ago
+		reset_sent_at < 2.hours.ago
 	end
 
-	  # Defines a proto-feed.
+	# Defines a proto-feed.
 	# See "Following users" for the full implementation.
 	def feed
-	Meter.where("user_id = ?", id)
+		Meter.where("user_id = ?", id)
 	end
 
 	private
