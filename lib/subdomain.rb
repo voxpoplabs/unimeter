e9be4f98731f33c889cd90a7b7e61fe606913c69
Jonathan Salis <jon@voxpoplabs.com>
2020-07-06 12:10:48 -0400

    Updating blog post creation

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,5 +1,5 @@
 class Subdomain
 	def self.matches?(request)
-		request.domain != "polimeter.org" && request.domain != "voxpop.local" || (request.subdomain.present? && request.subdomain != "www")
+		request.domain != "polimeter.org" && request.domain != "voxpop.internal" || (request.subdomain.present? && request.subdomain != "www")
 	end
 end
